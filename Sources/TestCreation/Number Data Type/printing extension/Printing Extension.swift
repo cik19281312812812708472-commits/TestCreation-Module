f89d374f2af5a6cb98bd03450c7ae1239196b390
Pilot010 <cik19281312812812708472@gmail.com>
2026-04-22 15:26:34 -0700

    printing extentions

---
+++ Sources/TestCreation/Number Data Type/printing extension/Printing Extension.swift
@@ -0,0 +1,25 @@
+//
+//  Printing Extension.swift
+//  TestCreation
+//
+//  Created by Desire on 2026-04-22.
+//
+
+extension Number: CustomStringConvertible {
+    
+    public var description: String {
+        
+        var prefix: String = ""
+        
+        if sign == .negative {
+            prefix = "-"
+        }
+        
+        return "\(prefix)\(value)"
+        
+    }
+    
+    
+    
+    
+}
