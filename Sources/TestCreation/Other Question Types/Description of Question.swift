dff3d6d506f03a672287e4a0d538fc674556a1b2
Pilot010 <cik19281312812812708472@gmail.com>
2026-06-11 17:45:16 -0700

    add creator ID

--- Sources/TestCreation/Other Question Types/Description of Question.swift
+++ Sources/TestCreation/Other Question Types/Description of Question.swift
@@ -5,12 +5,14 @@
 //  Created by Desire on 2026-06-08.
 //
 
+import Foundation
 
 @available(macOS 10.15, *)
 public struct DescriptionOfQuestion: Codable {
     
     
     //more description will be added
+    public var creator: UUID
     public var questionName: String
     public var questionText: String
     public var questionAnswer: String
@@ -18,7 +20,7 @@ public struct DescriptionOfQuestion: Codable {
     
    
     public init(question: Question) {
-        
+        self.creator = question.packageOwner
         self.questionName = question.questionName
         self.questionText = question.questionText
         self.questionAnswer = question.questionAnswer
