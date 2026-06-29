cd83c3b9134f58babdd9e40b2e37429a1dea6cab
Pilot010 <cik19281312812812708472@gmail.com>
2026-06-29 11:23:01 -0700

    added find packagecode

---
+++ Sources/TestCreation/Package Type/Array Package extension.swift
@@ -0,0 +1,25 @@
+//
+//  Array Package extension.swift
+//  TestCreation
+//
+//  Created by Desire on 2026-06-29.
+//
+
+
+@available(macOS 10.15, *)
+extension Array where Element: Package {
+    
+    func findPackage(_ id: UUID) -> (any Package)? {
+        
+        
+        let package: [any Package] = self.filter { $0.id == id}
+        
+        guard package.count > 0 else {
+            return nil
+        }
+        
+        return package[0]
+    }
+    
+    
+}
