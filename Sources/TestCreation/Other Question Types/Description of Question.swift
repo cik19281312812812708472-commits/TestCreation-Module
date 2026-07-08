039dd6a25bc892e9e0ad706919a9bc07ec5d5e65
Pilot010 <cik19281312812812708472@gmail.com>
2026-07-08 11:01:35 -0700

    added question Index to a description of question.

--- Sources/TestCreation/Other Question Types/Description of Question.swift
+++ Sources/TestCreation/Other Question Types/Description of Question.swift
@@ -22,23 +22,28 @@ public struct DescriptionOfQuestion: Codable {
     ///What is already inputed into the question
     public var questionInput: String
     
+    ///This tracks the current question Index.
+    public var questionIndex: Int
     
-    public init(ownerInternalName: String, question: Question, extraDescription: String = "") {
+    
+    public init(ownerInternalName: String, question: Question, extraDescription: String = "", questionIndex: Int = 0) {
         self.creatorInternalName = ownerInternalName
         self.questionName = question.questionName
         self.questionText = question.questionText
         self.questionAnswer = question.questionAnswer
         self.questionInput = question.input
         self.extraDescription = extraDescription
+        self.questionIndex = questionIndex
     }
     
-    public init(ownerInternalName: String, questionName: String, questionText: String, questionAnswer: String, questionInput: String, extraDescription: String = "") {
+    public init(ownerInternalName: String, questionName: String, questionText: String, questionAnswer: String, questionInput: String, extraDescription: String = "", questionIndex: Int = 0) {
         self.creatorInternalName = ownerInternalName
         self.questionName = questionName
         self.questionText = questionText
         self.questionAnswer = questionAnswer
         self.questionInput = questionInput
         self.extraDescription = extraDescription
+        self.questionIndex = questionIndex
         
     }
     
